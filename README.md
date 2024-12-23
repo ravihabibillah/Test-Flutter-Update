d796ae06915cb19efd2d8e0106b79890cd29df01
Muhammad Ravi Habibillah <ravihabibillah44@gmail.com>
2024-12-23 13:51:39 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# test_flutter_update_3_27
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/test_flutter_update_3_27/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.test_flutter_update_3_27
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
